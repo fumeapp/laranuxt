659579113881c1ff03cd58e363bae38f7f2fb977
kevin olson <acidjazz@gmail.com>
2018-12-25 19:36:18 -0800

    🎨 example modal progress.. needs fixing

--- resources/components/UserCard.vue
+++ resources/components/UserCard.vue
@@ -1,26 +1,15 @@
 <template lang="pug">
-.card
-  .card-content
+.box
+  .content
     article.media
       figure.media-left
         figure.image.is-64x64
           img(:src="user.avatar")
       .media-content
-        .content
-          div
-            strong {{ user.name }}
-          div
-            small {{ user .email }}
-  footer.card-footer
-    a.card-footer-item
-      span.icon: i.mdi.mdi-account-card-details
-      span Details
-    a.card-footer-item.has-text-danger
-      span.icon: i.mdi.mdi-delete
-      span Delete
-    a.card-footer-item.has-text-info
-      span.icon: i.mdi.mdi-login
-      span Login As
+        div
+          strong {{ user.name }}
+        div
+          small {{ user .email }}
 </template>
 
 <script>
--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -1,21 +1,28 @@
 <template lang="pug">
-#Users.page
+#Examples.page
   .container
     section.section
+      .buttons
+        .button(@click="modal_open")
+          span Example Modal
+    section.section
       .columns.is-multiline
-        .column.is-one-third(v-for="user in users")
+        .column(v-for="user in users")
           UserCard(:user="user")
+  ModalExample(v-if="is_modal",:close="modal_close")
 </template>
 
 <script>
 import UserCard from '@/components/UserCard'
+import ModalExample from '@/components/ModalExample'
 export default {
 
-  components: { UserCard },
+  components: { UserCard, ModalExample },
 
   data () {
     return {
       users: [],
+      is_modal: false,
     }
   },
 
@@ -26,8 +33,13 @@ export default {
   methods: {
     async get () {
       this.users = (await this.$axios.get('example')).data.data
-    }
+    },
+    modal_open () {
+      this.is_modal = true
+    },
+    modal_close () {
+      this.is_modal = false
+    },
   },
-
 }
 </script>
