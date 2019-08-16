5ad026400a89b2e684bd1a92296ceed86ff43b5d
acidjazz <acidjazz@gmail.com>
2019-08-16 17:00:26 -0500

    🔧 various script updates

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
-.container.p-8
+.container.p-2.lg_p-8
+  .m-8
+    img(src="/logo.svg").w-32.h-32.mx-auto.ani-zi
   .flex.flex-wrap.-p-4
-    .w-1_3.p-4(v-for="user, index in users")
-      .p-2.border.border-gray.rounded.shadow.tran-shadow.hover_shadow-lg.ani-sil(:class="`ani-d-${index+1}`")
+    .w-full.lg_w-1_3.p-4(v-for="user, index in users")
+      .p-2.border.border-gray.rounded.shadow.tran-shadow.hover_shadow-lg.ani-sil(:class="`ani-d-${index}`")
         .flex
           img(:src="user.avatar").rounded-full.w-20.h-20.m-2.bg-gray-600.ani-sil.ani-d-1
           .text.ml-2.mt-2.ani-sil.ani-d-2
