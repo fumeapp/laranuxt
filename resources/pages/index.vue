70fcfd245e96ee429ff9d6a5658d341b04315c0b
kevin olson <acidjazz@gmail.com>
2019-08-29 20:00:27 -0500

    🎨 better loading state

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -3,15 +3,24 @@
   .m-8.text-5xl.flex.justify-center
     a(href="https://github.com/acidjazz/laranuxt",target="_new")
       img.w-32.h-32(src="/laranuxt.png")
-  .flex.flex-wrap.-p-4
-    .w-full.lg_w-1_3.p-4(v-for="user, index in users")
-      .p-2.border.border-gray.rounded.shadow.tran-shadow.hover_shadow-lg.ani-sil(:class="`ani-d-${index}`")
+  .flex.flex-wrap.-p-4(v-if="users.length < 1")
+    .w-full.lg_w-1_3.p-4(v-for="i in 9")
+      .p-2.border.border-gray.rounded.shadow.tran-shadow.hover_shadow-lg
         .flex
-          img(:src="user.avatar").rounded-full.w-20.h-20.m-2.bg-gray-600.ani-sil.ani-d-1
-          .text.ml-2.mt-2.ani-sil.ani-d-2.truncate
-            .text-lg {{ user.name }}
-            .text-gray-600 {{ user.email }}
-            .text-gray-600 {{ user.phone }}
+          .rounded-full.w-20.h-20.m-2.bg-gray-100
+          .text.ml-2.mt-2.truncate
+            .text-lg.bg-gray-100.mb-1.w-20 &nbsp;
+            .bg-gray-100.mb-1.w-32 &nbsp;
+            .bg-gray-100.mb-1.w-40 &nbsp;
+  .flex.flex-wrap.-p-4(v-else)
+    .w-full.lg_w-1_3.p-4(v-for="user, index in users")
+      .p-2.border.border-gray.rounded.shadow.tran-shadow.hover_shadow-lg
+        .flex
+          img(:src="user.avatar").rounded-full.w-20.h-20.m-2.bg-gray-600.ani-fi.ani-d-1
+          .text.ml-2.mt-2.truncate
+            .text-lg.mb-1.ani-fi {{ user.name }}
+            .text-gray-600.mb-1.ani-fi.ani-d-1 {{ user.email }}
+            .text-gray-600.mb-1.ani-fi.ani-d-2 {{ user.phone }}
   .text-center
     span provided by endpoint
     span &nbsp;
