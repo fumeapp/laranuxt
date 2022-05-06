6e9302520a40f616e09cfec257b5fe3253c2c2ea
kevin olson <acidjazz@gmail.com>
2022-05-05 21:58:17 -0500

    ⬆️ add cleaner echo client

--- client/plugins/echo.client.ts
+++ client/plugins/echo.client.ts
@@ -2,41 +2,34 @@ import { defineNuxtPlugin, useNuxtApp, useRuntimeConfig } from '#app'
 import Echo from 'laravel-echo'
 import Pusher from 'pusher-js'
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig().public
   const { $api } = useNuxtApp()
-  window.Pusher = Pusher
-  const setEcho = () => {
-    if (!$api.token.value) return
-    const echo = new Echo({
-      broadcaster: 'pusher',
-      key: config.pusherAppKey,
-      cluster: 'mt1',
-      authEndpoint: `${config.apiURL}/broadcasting/auth`,
-      forceTls: true,
-      encrypted: true,
-      auth: {
-        headers: {
-          Accept: 'application/json',
-          Authorization: `Bearer ` + $api.token.value,
-        },
-      },
-    })
-    nuxtApp.provide('echo', echo)
-  }
-  if ($api.token.value) setEcho()
-  else $api.callback = setEcho
 
+  window.Pusher = Pusher
+  return {
+    provide: {
+      echo: new Echo({
+        broadcaster: 'pusher',
+        key: config.pusherAppKey,
+        cluster: config.pusherAppCluster,
+        authEndpoint: `${config.apiURL}/broadcasting/auth`,
+        forceTls: true,
+        encrypted: true,
+        auth: {
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ` + $api.token.value,
+          },
+        },
+      }),
+    },
+  }
 })
+
 
 declare module '#app' {
   interface NuxtApp {
-    $echo: typeof Echo
-  }
-}
-
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $echo: typeof Echo
+    $echo: Echo
   }
 }
