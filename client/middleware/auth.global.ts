e367b25d994108e755f6137457e4997e2149be76
tcampbPPU <tcamp022@gmail.com>
2023-01-31 15:12:13 -0500

    Fix Magic Link Auth Option

--- client/middleware/auth.global.ts
+++ client/middleware/auth.global.ts
@@ -1,5 +1,10 @@
-export default defineNuxtRouteMiddleware(to => {
+export default defineNuxtRouteMiddleware((to, from) => {
+
   const api = useApi()
-  if (api && to.path !== api.config.redirect.logout && to.path !== '/invalid' && api.loggedIn.value === false)
+  if (api && to.path !== api.config.redirect.logout && to.path !== '/invalid' && api.loggedIn.value === false) {
+    if (to.meta.middleware === 'guest') return
+    if (from.meta.middleware === 'guest') return
+
     return navigateTo(api.config.redirect.logout)
+  }
 })
